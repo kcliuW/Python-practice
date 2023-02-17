5f6e49af41369adc1ab2e4450e64b9adae6a3efc
kcliuW <williamkcliuinhk@gmail.com>
2023-02-17 16:53:13 +0800

    Feb 17

--- list intro.py
+++ list intro.py
@@ -20,3 +20,60 @@ word = ['H','O','L','I','D','A','Y']
 print(word [:5])
 print(word [1:5])
 print(word [3:])
+
+'''
+x = 'CHINESE'
+lista = list((x))
+print(lista)
+'''
+
+num = [[25, 58, 66],[21, 97, 36]]
+print(num[0])
+print(num[1][1])
+
+#  list delete
+L = [51,82,77,48,35,66,28,46,99]
+del L[6]
+print(L)
+
+L = [51,82,77,48,35,66,28,46,99]
+del L[1:4]
+print(L)
+
+print("happy" in ["good", "happy", "please"])
+
+print("sad" not in ["good", "happy", "please"])
+
+# List append
+word = ["red", "yellow", "green"]
+word.append("blue")
+print(word)
+
+# List insert
+word = ["red", "yellow", "green"]
+word.insert(2,"blue")
+print(word)
+
+# List remove
+word = ["red", "yellow", "green"]
+word.remove("red")
+print(word)
+
+# List pop
+word = ["red", "yellow", "green"]
+word.pop(2)
+print(word)
+
+# List sort
+word = ["red", "yellow", "green"]
+word.sort()
+print(word)
+
+# List reverse
+word = ["red", "yellow", "green"]
+word.reverse()
+print(word)
+
+# List length
+word = ["red", "yellow", "green"]
+print(len(word))
