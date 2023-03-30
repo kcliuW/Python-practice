70e56414ae49905c6f2e2df9b16f3118df4fab55
kcliuW <williamkcliuinhk@gmail.com>
2023-03-31 01:37:19 +0800

    March 31 update

---
+++ python step by step ex-3-1.py
@@ -0,0 +1,68 @@
+''' Practice Question '''
+
+'''
+Question 4
+'''
+#print("6*(24/a + (5+a)/b)=",c)
+x = 5
+y = 4
+z = 7*(23/x + (y+8)/x)
+print("7*(23/x + (y+8)/x)=",z)
+
+'''
+Question 5
+'''
+x = 19
+y = 23
+print(x > y or x == y and not y)
+
+'''
+Question 6
+'''
+a= 20; b = 5; c= 16
+print(a/b)
+print((a*b)*(b-15)/5+c*3)
+
+'''
+Question 7
+'''
+num = 10
+num *= 7
+print(num)
+num //= 4
+print(num)
+num %= 9
+print(num)
+
+'''
+Question 8
+'''
+a,b,c = 3,5,7
+print(a!=b and not b<c and c>a)
+print(not(a!=b)and(a>=b))
+
+'''
+Question 9
+'''
+x = 25
+y = 78
+
+print(x > y and x == y)
+
+'''
+Question 10
+
+'''
+
+a = 5
+b = 4
+c = 3
+
+x = a + b + c
+print("{}".format(x))
+a += c
+print("a={0}".format(a,b))
+a //= b
+print("a={0}".format(a,b))
+a %= c
+print("a={0}".format(a,b))
--- python step by step ex-4.py
+++ python step by step ex-4.py
@@ -1,68 +1,70 @@
-''' Practice Question '''
+'''
+Question 3
+'''
+# 以下程式請改成表達運算式只要簡單一行程式就能達成同樣的目的.
+x = int(input("Please input a number: "))
+if x % 2 == 0:
+    y = "偶數"
+else:
+    y = "奇數"
+print('{0}'.format(y))
+
+# Answer
+a = int(input("Please input a number: "))
+print('{0}'.format("偶數" if a % 2 == 0 else "奇數")) # 表達運算式
 
 '''
 Question 4
 '''
-#print("6*(24/a + (5+a)/b)=",c)
-x = 5
-y = 4
-z = 7*(23/x + (y+8)/x)
-print("7*(23/x + (y+8)/x)=",z)
+# 請設計一程式讓使用者輸入一整數, 並判斷是否為 5 或 7 的倍數, 不過
+# 卻不能為 35 的倍數.
 
+num = int(input("請任意輸入一個整數: "))
+if num % 5 == 0 or num % 7 == 0: # 判斷是否為 5 或 7 的倍數
+    if num % 35 != 0:
+        print("符合所要的條件.")
+    else:
+        print("不符合所要的條件.") # 為 35 的倍數
+else:
+   print("不符合所要的條件.") 
+   
 '''
 Question 5
 '''
-x = 19
-y = 23
-print(x > y or x == y and not y)
+# 請寫出下列程式的輸出結果.
+number = 84
+if number % 4 == 0:
+    if number % 7 == 0:
+        print("{} 是 4 與 7 的 公倍數".format(number))
+else:
+        print("{} 不是 4 的倍數".format(number))
 
 '''
 Question 6
 '''
-a= 20; b = 5; c= 16
-print(a/b)
-print((a*b)*(b-15)/5+c*3)
+# 請問以下程式碼的執行結果?
+cost = 180
+if cost >= 125:
+    print("Expensive")
 
 '''
 Question 7
 '''
-num = 10
-num *= 7
-print(num)
-num //= 4
-print(num)
-num %= 9
-print(num)
+# 請問以下程式碼的執行結果?
+X = 121
+print("7的倍數" if x % 7 == 0 else "不是7的倍數")
 
 '''
 Question 8
 '''
-a,b,c = 3,5,7
-print(a!=b and not b<c and c>a)
-print(not(a!=b)and(a>=b))
-
+# 請問以下程式碼的執行結果?
+height = 180
+if height >= 175:
+    print("Tall")
+    
 '''
 Question 9
 '''
-x = 25
-y = 78
-
-print(x > y and x == y)
-
-'''
-Question 10
-
-'''
-
-a = 5
-b = 4
-c = 3
-
-x = a + b + c
-print("{}".format(x))
-a += c
-print("a={0}".format(a,b))
-a //= b
-print("a={0}".format(a,b))
-a %= c
-print("a={0}".format(a,b))
+# 請問以下程式碼的執行結果?
+Z = 20
+print("5的倍數" if x % 7 == 0 else "不是5的倍數")
