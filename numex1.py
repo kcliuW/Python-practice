8a2f7b6e8144e7153ec514a3f44b52c3fba3c3e4
kcliuW <110001601+kcliuW@users.noreply.github.com>
2023-08-07 02:34:48 +0800

    07Aug23

---
+++ numex1.py
@@ -0,0 +1,11 @@
+import numpy as np
+na = np.arange(1, 17).reshape(4, 4)
+print(na)
+print(na[2,3])
+print(na[1,1:3])
+print(na[1:3,2])
+print(na[1:3,1:3])
+print(na[::2,::2])
+print(na[:,2])
+print(na[1,:])
+print(na[:,:])
